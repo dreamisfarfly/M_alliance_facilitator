9745ac6fe84ac566f8b2f5a055f224e6b65f3825
dreamisfarfly <dreamisfarfly@gmail.com>
2022-10-19 17:00:39 +0800

    main

--- pages/my/withdraw.vue
+++ pages/my/withdraw.vue
@@ -1,8 +1,7 @@
 <template>
-  <!-- start withdraw -->
-  <view class="withdraw">
+  <Container>
     <!-- start bank-card -->
-    <view class="bank-card">
+    <view class="bank-card m-t-10 justify-horizontal-center">
       <view class="bank-card-info">
         <label class="bank-card-info-label">银行卡</label>
         <text class="bank-card-info-bank-number">400 40000 40000 4000</text>
@@ -14,9 +13,9 @@
     </view>
     <!-- end bank-card -->
     <!-- start withdraw-card -->
-    <view class="withdraw-card">
+    <view class="withdraw-card m-t-10">
       <view class="withdraw-card-name">提现金额</view>
-      <view class="withdraw-card-import">
+      <view class="withdraw-card-import horizontal-center ">
         <view class="withdraw-card-import-symbol">￥</view>
         <input
           class="withdraw-card-import-input"
@@ -28,12 +27,11 @@
         余额<text style="color: #333333">0.55元</text>
         <text style="color: #c78125; margin-left: 40rpx">全部提现</text>
       </view>
-      <view class="next-step">下一步</view>
+      <view class="next-step m-t-20 text-center">下一步</view>
     </view>
     <!-- end withdraw-card -->
-    <view class="withdraw-hint">预计2小时内到账</view>
-  </view>
-  <!-- end withdraw -->
+    <view class="hint m-t-15 text-center">预计2小时内到账</view>
+  </Container>
 </template>
 
 <script>
@@ -46,92 +44,70 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.withdraw {
-  padding: 0 30rpx;
-  padding-top: 20rpx;
-  .bank-card {
-    width: 100%;
-    box-sizing: border-box;
-    background: #ffffff;
-    box-shadow: 0 8rpx 16rpx 0 rgba(108, 119, 128, 0.05);
-    border-radius: 16rpx;
-    padding: 30rpx 48rpx 30rpx 32rpx;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    .bank-card-info {
-      .bank-card-info-label {
-        font-size: 28rpx;
-        font-weight: 400;
-        color: #333333;
-      }
-      .bank-card-info-bank-number {
-        margin-left: 70rpx;
-        font-size: 28rpx;
-        font-weight: 500;
-        color: #333333;
-      }
-    }
-    .bank-card-info-operation {
-      width: 17rpx;
-      height: 22rpx;
-    }
-  }
-  .withdraw-card {
-    margin-top: 20rpx;
-    background: #ffffff;
-    box-shadow: 0 8rpx 16rpx 0 rgba(108, 119, 128, 0.05);
-    border-radius: 16rpx;
-    padding: 46rpx 30rpx 40rpx 30rpx;
-    box-sizing: border-box;
-    .withdraw-card-name {
+.bank-card {
+  width: 100%;
+  background: #ffffff;
+  box-shadow: 0 8rpx 16rpx 0 rgba(108, 119, 128, 0.05);
+  border-radius: 16rpx;
+  padding: 30rpx 48rpx 30rpx 32rpx;
+  .bank-card-info {
+    .bank-card-info-label {
       font-size: 28rpx;
       font-weight: 400;
       color: #333333;
     }
-    .withdraw-card-import {
-      margin-top: 24rpx;
-      padding: 26rpx 0;
-      display: flex;
-      align-items: center;
-      border-bottom: 2rpx solid #f0f0f0;
-      box-sizing: border-box;
-      .withdraw-card-import-symbol {
-        width: 82rpx;
-        font-size: 48rpx;
-        font-weight: 600;
-        color: #333333;
-      }
-      .withdraw-card-import-input {
-        font-size: 48rpx;
-        font-weight: 400;
-      }
-    }
-    .withdraw-balance {
-      margin-top: 26rpx;
+    .bank-card-info-bank-number {
+      margin-left: 70rpx;
       font-size: 28rpx;
-      font-weight: 400;
-      color: #999999;
-    }
-    .next-step {
-      margin-top: 40rpx;
-      width: 100%;
-      height: 80rpx;
-      background: #c78125;
-      border-radius: 16rpx;
-      font-size: 32rpx;
-      font-weight: 600;
-      color: #ffffff;
-      text-align: center;
-      line-height: 80rpx;
+      font-weight: 500;
+      color: #333333;
     }
   }
-  .withdraw-hint {
-    margin-top: 30rpx;
-    text-align: center;
-    font-size: 24rpx;
+  .bank-card-info-operation {
+    width: 17rpx;
+    height: 22rpx;
+  }
+}
+.withdraw-card {
+  background: #ffffff;
+  box-shadow: 0 8rpx 16rpx 0 rgba(108, 119, 128, 0.05);
+  border-radius: 16rpx;
+  padding: 46rpx 30rpx 40rpx 30rpx;
+  .withdraw-card-name {
+    font-size: 28rpx;
+    font-weight: 400;
+    color: #333333;
+  }
+  .withdraw-card-import {
+    margin-top: 24rpx;
+    padding: 26rpx 0;
+    border-bottom: 2rpx solid #f0f0f0;
+    box-sizing: border-box;
+    .withdraw-card-import-symbol {
+      width: 82rpx;
+      font-size: 48rpx;
+      font-weight: 600;
+      color: #333333;
+    }
+    .withdraw-card-import-input {
+      font-size: 48rpx;
+      font-weight: 400;
+    }
+  }
+  .withdraw-balance {
+    margin-top: 26rpx;
+    font-size: 28rpx;
     font-weight: 400;
     color: #999999;
+  }
+  .next-step {
+    width: 100%;
+    padding: 18rpx 0;
+    background: #c78125;
+    border-radius: 16rpx;
+    font-size: 32rpx;
+    font-weight: 600;
+    color: #ffffff;
   }
 }
 </style>
